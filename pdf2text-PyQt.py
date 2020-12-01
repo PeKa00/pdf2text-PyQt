be3a8a8a9adf73030838e07816f91f530d383e54
Katharina Pernsteiner <katharina.pernsteiner@protonmail.com>
2020-12-01 21:42:59 +0100

    Add layouts

--- pdf2text-PyQt.py
+++ pdf2text-PyQt.py
@@ -3,7 +3,21 @@ from PyQt5.QtWidgets import *
 if __name__ == '__main__':
 	app = QApplication([])
 	window = QWidget()
+	
+	layoutH = QHBoxLayout()
+	layoutV1 = QVBoxLayout()
+	layoutV2 = QVBoxLayout()
 
+	layoutV1.addWidget(QPushButton("Add pdf"))
+	layoutV1.addWidget(QLabel("bla"))
+	layoutV2.addWidget(QPushButton('Export to txt'))
+	layoutV2.addWidget(QLabel('blabla'))
+
+	layoutH.addLayout(layoutV1)
+	layoutH.addLayout(layoutV2)
+
+	window.setLayout(layoutH)
+	
 	window.setWindowTitle("pdf2Text-PyQt")
 	window.show()
 
